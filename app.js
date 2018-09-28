094a960737c9e2f2bb0a5f2eee0117f5da42b264
wangtaofy <2352748511@qq.com>
2018-09-28 16:56:00 +0800

    commit

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('Hello nodeasdasdasdfds');
+// 引入hapi
+const Hapi = require('hapi');
+const config = require('./config');
+const routesHelloHapi = require('./routes/hello-hapi');
+
+require('env2')('./.env');
+const { env } = process;
+// console.log('env', process.env.PORT);
+
+const server = new Hapi.Server();
+// 配置服务器启动host与端口
+server.connection({
+  port: env.PORT,
+  host: env.HOST
+});
+
+const init = async () => {
+  server.route([
+    // 创建一个简单的hello hapi接口
+    ...routesHelloHapi
+  ]);
+
+  // 启动服务
+  await server.start();
+  console.log(`Server runing at: ${server.info.uri}`);
+}
+
+init();
---
+++ config/index.js
@@ -0,0 +1,5 @@
+// 环境配置
+module.exports = {
+  host: '127.0.0.1',
+  port: 3000
+}
